3dcdad0ca2819e64f0ce8080171fb7b381fe7b24
RichMo1906 <130684513+RichMo1906@users.noreply.github.com>
2023-04-21 00:49:52 -0400

    Add files via upload
    First Commit

---
+++ BruteForce.java
@@ -0,0 +1,17 @@
+package com.example.finalproject;
+
+public class BruteForce {
+    // slkjosriepfesfhuiwaruio
+    // 1 -> rkjaksdjkawijeq -> -28
+    // 2 -> wqieoiqweiuouww -> -22
+    // 3 -> qwi euoitr sxe -> 5
+    // 4 -> alpksdpoeajkses -> -2
+    // 5 -> this is awesome -> 47
+    // 5 -> awoi aeuis seeq -> 10
+    // 6 -> was kwe of wqes -> 21
+
+    // LanguageRules
+    // int evaluateLetterPair(String letterPair) -> returns -7 for wx, and +3 for of
+    // wx, fg, fx, xw -> non existent letter combinations in English -10
+    // of, is, the, a, etc. -> common words +5
+}
